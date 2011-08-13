c36ff53a687b43d826a89b1471b7f856ee85c760
Ivan Jovanovic <ivan.jovanovic@local.ch>
2011-08-13 10:03:42 +0200

    Finished Exercise 1.12

---
+++ 1.2/e-1.12.scm
@@ -0,0 +1,27 @@
+; Exercise 1.12.  The following pattern of numbers is called Pascal's triangle.
+;
+;                rows
+;                 1 | 1
+;                 2 | 1   1
+;                 3 | 1   2   1
+;                 4 | 1   3   3   1
+;                 5 | 1   4   6   4   1
+;                   --------------------
+;               cols  1   2   3   4   5
+; The numbers at the edge of the triangle are all 1, and each number inside
+; the triangle is the sum of the two numbers above it.
+; Write a procedure that computes elements of Pascal's triangle by means of a recursive process.
+;
+; -----------------------------------------------------------------
+
+
+
+(define (pascal row col)
+  (if (or (= col 1) (= col row))
+      1
+      (+ 
+        (pascal (- row 1) (- col 1))
+        (pascal (- row 1) col))))
+
+(display (pascal 10 5))
+(newline)
