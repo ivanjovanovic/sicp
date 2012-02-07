fb4dd4d7a0536cdc38b3bb4b88aac30ea03d5eb0
Ivan Jovanovic <ivan.jovanovic@gmail.com>
2012-02-08 00:11:46 +0100

    Very clever solution for detection of cycles in the linked list

--- 3.3/e-3.19.scm
+++ 3.3/e-3.19.scm
@@ -9,4 +9,46 @@
 ; dependent on the length of the list this will grow over time. 
 ; Its space growth is characterized by O(n) asymptotic approximation.
 
-(define (cycled? l))
+
+; There is indeed a very clever way to find the cycle in any part of the list.
+; We just have to make two pointers, point them to first element, and then go through
+; the list in a way that we increase first one to the next position and second one for two positions.
+; If there is a cycle in the list these two pointers will eventually meet, otherwise they will both
+; end in null end and exit the algorithm loop.
+
+(load "../helpers.scm")
+
+(define (cycled? l)
+  (if (null? (cdr l)) 
+    '#f
+    (let ((slow l)
+          (fast (cdr l)))
+      (define (recur)
+        (cond ((or (null? (cdr slow))
+                   (or (null? (cdr fast)) (null? (cddr fast))))
+               '#f)
+              ((eq? slow fast) '#t)
+              (else
+                (begin
+                  (set! slow (cdr slow))
+                  (set! fast (cddr fast))
+                  (recur)))))
+      (recur))))
+
+; no cycle in simple linked list
+(output (cycled? (list 1 2 3))) ; #f
+
+; build cycled list out of pairs
+(define first (cons 1 2))
+(define second (cons 1 2))
+(define third (cons 1 2))
+(define forth (cons 1 2))
+(define fifth (cons 1 2))
+
+(set-cdr! first second)
+(set-cdr! second third)
+(set-cdr! third forth)
+(set-cdr! forth fifth)
+(set-cdr! fifth second)
+
+(output (cycled? first)) ; #t
