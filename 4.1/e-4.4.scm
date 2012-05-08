8314b89b89dd23634cccd2e93373bfc00d587360
Ivan Jovanovic <ivan.jovanovic@gmail.com>
2012-05-08 23:17:07 +0200

    Added expressions derived from if clauses

--- 4.1/e-4.4.scm
+++ 4.1/e-4.4.scm
@@ -39,18 +39,51 @@
              (or-operands-eval-recur (cdr operands))))))
   (or-operands-eval-recur (cdr exp)))
 
-; these procedures are able to be expressed as drivatives where
+; installing them is easy
+(put 'and and-eval)
+(put 'or  or-eval)
+
+; they can be expressed as well as the derived from if expressions
+
+; and related procedures
+(define (and-eval exp env)
+  (eval (and->if exp) env))
+(define (and->if exp)
+  (expand-and-operands (cdr exp)))
+(define (expand-and-operands operands)
+  (let ((first (car operands))
+        (rest (cdr operands)))
+    (if (null? rest)
+      (make-if first
+               first
+               'false)
+      (make-if first
+               (expand-and-operands rest)
+               'false)))
+
+; or related procedures
+(define (or-eval exp env)
+  (eval (or->if exp) env))
+(define (or->if exp)
+  (expand-or-operands (cdr exp)))
+(define (expand-or-operands operands)
+  (let ((first (car operands))
+        (rest (cdr operands)))
+    (if (null? first)
+      'false
+      (make-if first
+               'true
+               (expand-or-operands rest)))))
+
+; these procedures are able to be expressed as derivatives of themselves
 (define (or-eval exp env)
   (let* ((operands (cdr exp))
-         (mapped-operands (map (lambda (op) (not op)))))
-    (and-eval (cons (car exp) mapped-operands))))
+         (mapped-operands (map (lambda (op) (not (eval op env))))))
+    (and-eval (cons (car exp) mapped-operands) env)))
 
 
 (define (and-eval exp env)
   (let* ((operands (cdr exp))
-         (mapped-operands (map (lambda (op) (not op)))))
-    (or-eval (cons (car exp) mapped-operands))))
+         (mapped-operands (map (lambda (op) (not (eval op env))))))
+    (or-eval (cons (car exp) mapped-operands) env)))
 
-; installing them is easy
-(put 'and and-eval)
-(put 'or  or-eval)
